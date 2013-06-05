097bb2a8790ec5135799269eb93fa124a2ab7189
Fred Medlin <fred@twotoasters.com>
2013-06-05 16:41:06 -0400

    Get/Display auth token and update tools

--- src/main/java/org/example/accountexplorer/AccountActivity.java
+++ src/main/java/org/example/accountexplorer/AccountActivity.java
@@ -1,35 +1,124 @@
 package org.example.accountexplorer;
 
 import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import org.example.accountexplorer.R;
 
 public class AccountActivity extends Activity {
-	
-	Account account;
+
+	private final String TAG = "AccountActivity";
+	private final int AUTH_REQUEST = 0;
+
+	private AccountManager manager;
+	private Account account;
+	private TextView nameText;
+	private TextView statusText;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		manager = AccountManager.get(this);
 		account = getIntent().getParcelableExtra("account");
+
 		setTitle(account.type);
 		setContentView(R.layout.activity_account);
-		setAccountName(account.name);
+		setupViews();
 	}
 
-	private void setAccountName(String name) {
-		TextView nameText = (TextView) findViewById(R.id.text_name);
-		nameText.setText(name);
+	private void setupViews() {
+		nameText = (TextView) findViewById(R.id.text_name);
+		statusText = (TextView) findViewById(R.id.text_status);
+
+		nameText.setText(account.name);
 	}
 
 	public void onAuthenticateClick(View v) {
-		TextView name = (TextView) findViewById(R.id.text_name);
-		name.setText("Authenticating!");
+		authenticate();
+	}
+
+	public void authenticate() {
+		Log.d(TAG, "authenticate");
+		try {			
+			manager.getAuthToken(
+				account,
+				"android",					// TODO: Should be authenticator-dependent string (authorization_code) ??
+				new Bundle(),
+				this,
+				new OnTokenAcquired(),
+				new Handler(new OnError())	// main handler thread
+			);
+
+		}
+		catch (Exception e) {
+			logError("authenticate:", e);
+		}
 
 	}
 
+	private class OnError implements Handler.Callback {
+		public boolean handleMessage(Message msg) {
+			Log.d(TAG, "OnError: " + msg.toString());
+			return true;
+		}
+	}
+
+	private void handleBundleResult(Bundle bundle) {
+		Log.v(TAG, bundle.toString());
+		if (bundle.containsKey(AccountManager.KEY_INTENT)) {
+			Intent launch = (Intent) bundle.getParcelable(AccountManager.KEY_INTENT);
+			if (launch != null) {
+				Log.v(TAG, "Starting activity with intent: " + launch.toString());
+				startActivityForResult(launch, AUTH_REQUEST);
+				return;
+			}
+		}
+
+		if (bundle.containsKey(AccountManager.KEY_AUTHTOKEN)) {
+			statusText.setText("authtoken: " + bundle.getString(AccountManager.KEY_AUTHTOKEN));
+		}
+		else {
+			statusText.setText("WTF");
+			Log.v(TAG, bundle.toString());
+		}		
+	}
+
+	private class OnTokenAcquired implements AccountManagerCallback<Bundle> {
+		public void run(AccountManagerFuture<Bundle> future) {
+			Log.d(TAG, "callback : run");
+			try {
+				handleBundleResult(future.getResult());
+			}
+			catch(Exception e) {
+				logError("callback:", e);
+			}
+		}
+	};
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == AUTH_REQUEST) {
+			if (resultCode == Activity.RESULT_OK) {
+				Log.e(TAG, "activity result : success");
+				authenticate();
+			}
+			else {
+				Log.e(TAG, "activity result : failed");
+			}
+		}
+	}
+
+	private void logError(String title, Exception e) {
+		Log.e(TAG, title, e);
+	}
 }
